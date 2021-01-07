eb8b1961afad32d48ab82b7aa2d118ed79374611
Carlos <carlosoftwar@gmail.com>
2021-01-07 19:47:28 -0300

    class exemple

---
+++ first steps/Filme.js
@@ -0,0 +1,25 @@
+class Filme {
+     constructor(){
+        this.titulo = '';
+        this.ano = '2000';
+        this.genero = ' ';
+        this.diretor = ' ';
+        this.atores = [];
+        this.duracao = 0;
+     }
+
+     Reproduzir(){
+        console.log("Reproduzindo");
+     }
+     Pausar(){
+        console.log("Pause")
+     }
+     Avancar(){
+         console.log("Avança")
+     }
+     Fechar(){
+         console.log("Fechando")
+     }
+
+
+}

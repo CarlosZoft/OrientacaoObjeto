edf3b7c0b70484091b2579b022ebf56f351335b6
Carlos <carlosoftwar@gmail.com>
2021-01-08 14:56:33 -0300

    changes values of atributes

--- first steps/Filme.js
+++ first steps/Filme.js
@@ -1,11 +1,11 @@
 class Filme {
-     constructor(){
-        this.titulo = '';
-        this.ano = 2000;
-        this.genero = ' ';
-        this.diretor = ' ';
-        this.atores = [];
-        this.duracao = 0;
+     constructor(titulo, ano, genero, diretor,duracao,...atores){
+        this.titulo = titulo;
+        this.ano = ano;
+        this.genero = genero;
+        this.diretor = diretor;
+        this.duracao = duracao;
+        this.atores = atores;
      }
 
      Reproduzir(){
@@ -24,15 +24,9 @@ class Filme {
 
 }
 
-var vingadores = new Filme();
-
-console.log("titulo = " + vingadores.titulo)
-
-var logan = new Filme();
-
-console.log("Ano = " + logan.ano)
-var harryPotter = new Filme();
-
-harryPotter.Pausar();
-harryPotter.Avancar();
-harryPotter.Fechar();
+var vingadores = new Filme("Vingadores",2014,"ação");
+var logan = new Filme("logan", 2017, "ação");
+var harryPotter = new Filme("Harry potter e a Criança Amaldiçoada", 2016, "ficção/fantasia");
+console.log(vingadores)
+console.log(logan)
+console.log(harryPotter)

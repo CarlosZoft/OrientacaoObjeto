c5085759303886b118821897451a32366645ae77
Carlos <carlosoftwar@gmail.com>
2021-01-08 15:50:19 -0300

    static method

---
+++ first steps/Calculadora.js
@@ -0,0 +1,19 @@
+class Calculadora{
+
+    static Somar(a,b){
+        console.log(a+b);
+    }
+
+    static Subtrair(a,b){
+        console.log(a-b);
+    }
+}
+/*
+var calculador = new Calculadora();
+
+calculador.Somar(2,9);
+calculador.Subtrair(5,5);
+*/
+
+Calculadora.Somar(2,9)
+Calculadora.Subtrair(5,5)

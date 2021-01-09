e838c95af06c045cd002d685b4c56daea2b935bb
Carlos <carlosoftwar@gmail.com>
2021-01-08 22:13:16 -0300

    Main

--- converter_excelToPDF/converter/app.js
+++ converter_excelToPDF/converter/app.js
@@ -1,8 +1,25 @@
 var Reader = require('./Reader')
+var Processor = require('./Processor')
+var Table = require('./Table')
+var HtmlParser = require('./HtmlParser')
+var Writer = require('./Writer')
+var WriterPDF = require('./WriterPDF')
+
 var leitor = new Reader();
+var escritor = new Writer();
 
 async function main (){
     var data = await leitor.Read("./planilha.csv");
+    var dataProcessor = Processor.Process(data);
+    var users = new Table(dataProcessor);
+    console.log(users.rows)
+
+    var html = await HtmlParser.Parse(users)
+
+    escritor.Write(Date.now() + ".html",html)
+    WriterPDF.Write(Date.now() + ".PDF",html)
+    
 }
 
 main();
+
